b41141ce87a71648f4a9b1cb8b14aea725be1634
stan wielga <svwielga4@gmail.com>
2018-03-11 22:12:18 -0700

    working through number koans

--- lib/koans/03_numbers.ex
+++ lib/koans/03_numbers.ex
@@ -5,64 +5,64 @@ defmodule Numbers do
   @intro "Why is the number six so scared? Because seven eight nine!\nWe should get to know numbers a bit more!"
 
   koan "Is an integer equal to its float equivalent?" do
-    assert 1 == 1.0 == ___
+    assert 1 == 1.0 == true
   end
 
   koan "Is an integer threequal to its float equivalent?" do
-    assert 1 === 1.0 == ___
+    assert 1 === 1.0 == false
   end
 
   koan "Revisit divison with threequal" do
-    assert 2 / 2 === ___
+    assert 2 / 2 === 1.0
   end
 
   koan "Another way to divide" do
-    assert div(5, 2) == ___
+    assert div(5, 2) == 2
   end
 
   koan "What remains or: The Case of the Missing Modulo Operator (%)" do
-    assert rem(5, 2) == ___
+    assert rem(5, 2) == 1
   end
 
   koan "Other math operators may produce this" do
-    assert 2 * 2 === ___
+    assert 2 * 2 === 4
   end
 
   koan "Or other math operators may produce this" do
-    assert 2 * 2.0 === ___
+    assert 2 * 2.0 === 4.0
   end
 
   koan "Two ways to round, are they exactly the same?" do
-    assert Float.round(1.2) === round(1.2) == ___
+    assert Float.round(1.2) === round(1.2) == false
   end
 
   koan "Release the decimals into the void" do
-    assert trunc(5.6) === ___
+    assert trunc(5.6) === 5
   end
 
   koan "Are you odd?" do
-    assert Integer.is_odd(3) == ___
+    assert Integer.is_odd(3) == true
   end
 
   koan "Actually you might be even" do
-    assert Integer.is_even(4) == ___
+    assert Integer.is_even(4) == true
   end
 
   koan "Let's grab the individual digits in a list" do
     individual_digits = Integer.digits(58127)
-    assert individual_digits == ___
+    assert individual_digits == [5, 8, 1, 2, 7]
   end
 
   koan "Oh no! I need it back together" do
     number = Integer.undigits([1, 2, 3, 4])
 
-    assert number == ___
+    assert number == 1234
   end
 
   koan "Actually I want my number as a string" do
     string_digit = Integer.to_string(1234)
 
-    assert string_digit == ___
+    assert string_digit == "1234"
   end
 
   koan "The meaning of life in hexidecimal is 2A!" do
